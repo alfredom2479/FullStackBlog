0b8af03cbbe055c4a24d89edec1a1c7cc148c4ae
pastaBoi07 <46807055+pastaBoi07@users.noreply.github.com>
2023-09-08 15:31:23 -0400

    fixed errorMiddleware so that its more secure and accurate

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,12 +1,21 @@
+const notFound = (req,res,next) =>{
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+}
 const errorHandler = (err,req,res,next) => {
-  const statusCode = res.statusCode ? res.statusCode : 500;
+  let statusCode = res.statusCode === 200 ? res.statusCode : 500;
+  let message = err.message;
 
-  res.status(statusCode);
+  if(err.name ==="CastError" && err.kind === "ObjectId"){
+    statusCode = 404;
+    message = "Resource not found";
+  }
 
-  res.json({
-    message: err.message,
+  res.status(statusCode).json({
+    message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack
   });
 };
 
-export default errorHandler
+export { errorHandler,notFound}
